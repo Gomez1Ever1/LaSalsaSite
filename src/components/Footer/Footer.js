68652b6354cfe1e8816bec2ea52d7944419c5206
Ever's PC <gomezsantiagoeverardo@gmail.com>
2021-04-15 01:23:03 -0700

    slight change

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,7 +12,8 @@ export default function Footer() {
                         <div>
                             <h3>Call Us Today!</h3>
                             <h3>(805) 975-8021</h3>
-                            <p>	&#169; La Salsa Market LLC. We do not save, use, or sell any information. All rights reserved</p>
+                            <p>	&#169; La Salsa Market LLC. All rights reserved</p>
+                            <p> We do not save, use, or sell any information.</p>
                         </div>
                     </Navbar.Brand>
                 </Container>
